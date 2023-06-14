74807e1e2796de707910effdbfc90c55bccad0ad
tiwari-2612 <uttu3101@gmail.com>
2023-06-14 14:41:19 +0530

    second Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,54 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main{
+import java.io.*;
+import java.util.*;
+
+public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println(" WElcome to sharda Swiggy delivery app");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Validate user credentials
+
+            // Display menu options
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Select your Food Type:");
+        System.out.println("Veg for Vegetarian food");
+        System.out.println("Non Veg for Non Vegetarian food");
+            String dish = sc.nextLine();
+            if (dish.equalsIgnoreCase("Veg")) {
+                System.out.println("Here's the List of Some Famous Restaurants:");
+                info("C:\\Users\\uttu3\\OneDrive\\Desktop\\VegMenu.csv");
+            } else if (dish.equalsIgnoreCase("Non veg")) {
+                System.out.println("Here's the List of Some Famous Restaurants:");
+                info("C:\\Users\\uttu3\\OneDrive\\Desktop\\Nonveg.csv");
+            }
+
+            // Process the user's choice
+
         }
+        private static void info (String filePath){
+
+            String line = "";
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(filePath));
+                while ((line = reader.readLine()) != null) {
+                    String[] row = line.split(",");
+                    for (String i : row) {
+                        System.out.printf("%-30s", i);
+                    }
+                    System.out.println();
+                }
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+
+
+            }
+
+        }
+
+
     }
-}
+
