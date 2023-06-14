3b8f50985d3d80f7124ff03f59ef81983431c864
tiwari-2612 <uttu3101@gmail.com>
2023-06-14 15:23:59 +0530

    New_commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,54 +1,84 @@
 package org.example;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" WElcome to sharda Swiggy delivery app");
-
+        System.out.println("Welcome to Sharda Swiggy Delivery App");
 
         // Validate user credentials
 
-            // Display menu options
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Select your Food Type:");
-        System.out.println("Veg for Vegetarian food");
-        System.out.println("Non Veg for Non Vegetarian food");
-            String dish = sc.nextLine();
-            if (dish.equalsIgnoreCase("Veg")) {
-                System.out.println("Here's the List of Some Famous Restaurants:");
-                info("C:\\Users\\uttu3\\OneDrive\\Desktop\\VegMenu.csv");
-            } else if (dish.equalsIgnoreCase("Non veg")) {
-                System.out.println("Here's the List of Some Famous Restaurants:");
-                info("C:\\Users\\uttu3\\OneDrive\\Desktop\\Nonveg.csv");
-            }
+        // Display menu options
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select your Food Type:");
+        System.out.println("1. Vegetarian food");
+        System.out.println("2. Non Vegetarian food");
+        System.out.println("3. Chinese");
+        int dish = scanner.nextInt();
 
-            // Process the user's choice
-
-        }
-        private static void info (String filePath){
-
-            String line = "";
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(filePath));
-                while ((line = reader.readLine()) != null) {
-                    String[] row = line.split(",");
-                    for (String i : row) {
-                        System.out.printf("%-30s", i);
-                    }
-                    System.out.println();
-                }
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-
-
-            }
+        if (dish==1) {
+            System.out.println("Here's the List of Dishes we offer");
+            displayMenu("C:\\Users\\uttu3\\OneDrive\\Desktop\\VegMenu.csv");
+        } else if (dish==2) {
+            System.out.println("Here's the List of Dishes we offer");
+            displayMenu("C:\\Users\\uttu3\\OneDrive\\Desktop\\Nonveg.csv");
+        } else if (dish==3) {
+            System.out.println("Here's the List of Dishes we offer");
+            displayMenu("C:\\Users\\uttu3\\OneDrive\\Desktop\\Chinese.csv");
 
         }
+        else {
+            System.out.println("Invalid choice!");
+        }
+
+        System.out.println("Do you want to Add Breads");
+        System.out.println("Type Yes to continue, No otherwise");
+        String disha = scanner.nextLine();
+
+        if (disha.equalsIgnoreCase("Yes")) {
+            displayMenu("C:\\Users\\uttu3\\OneDrive\\Desktop\\Bread.csv");
+        }
+
+        System.out.println("Do you want to Add Rice ");
+        System.out.println("Type Yes to continue, No otherwise");
+        String dishes = scanner.nextLine();
+
+            if (dishes.equalsIgnoreCase("Yes"))
+            {
+                displayMenu("C:\\Users\\uttu3\\OneDrive\\Desktop\\Rice.csv");
+            }
+
+        System.out.println("Sweetness is the key to get people, Do you want some good sweet dishes in your menu");
+        System.out.println("Type Yes to continue, No otherwise");
+        String dishe = scanner.nextLine();
+        if (dishe.equalsIgnoreCase("Yes"))
+        {
+            displayMenu("C:\\Users\\uttu3\\OneDrive\\Desktop\\Desert.csv");
+        }
 
 
+
+
+
+        scanner.close();
     }
 
+    private static void displayMenu(String csvFilePath) {
+        String line = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader(csvFilePath))) {
+            while ((line = reader.readLine()) != null) {
+                String[] row = line.split(",");
+                for (String item : row) {
+                    System.out.printf("%-30s", item);
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading the menu file: " + e.getMessage());
+        }
+    }
+}
